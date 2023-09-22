ac39126ed5979c783516acb449ada0ef3bd13e10
Drashti <Drashti.Sikligar@americanhipinstitute.org>
2023-09-22 09:14:36 -0500

    data formatting hopefully works

--- mapdata.py
+++ mapdata.py
@@ -1,10 +1,8 @@
 #read in data from csv
-#use dict to map male to false, female to true
-#use dict to map all the xray shit yes to true, no to false
 import numpy as np
 import pandas as pd
 
-print("formatting data")
+print("running my stupid baka code")
 data = pd.read_csv('data_414.csv', low_memory=False)
 #data = pd.DataFrame(data)
 data = data.dropna()
@@ -28,41 +26,44 @@ data.loc[data['MRI Gluteus medius pathology'] == 'Yes', 'MRI Gluteus medius path
 data.loc[data['MRI Generalized chondral damage'] == 'No', 'MRI Generalized chondral damage'] = False
 data.loc[data['MRI Generalized chondral damage'] == 'Yes', 'MRI Generalized chondral damage'] = True
 
-data.loc[data['MRI Localized chondral defect (not degenerative)'] == 'Male', 'Sex'] = 0
-data.loc[data['MRI Localized chondral defect (not degenerative)'] == 'Female', 'Sex'] = 1
+data.loc[data['MRI Localized chondral defect (not degenerative)'] == 'No', 'MRI Localized chondral defect (not degenerative)'] = False
+data.loc[data['MRI Localized chondral defect (not degenerative)'] == 'Yes', 'MRI Localized chondral defect (not degenerative)'] = True
 
-data.loc[data['MRI Subchondral cyst - Femur central compartment'] == 'Male', 'Sex'] = 0
-data.loc[data['MRI Subchondral cyst - Femur central compartment'] == 'Female', 'Sex'] = 1
+data.loc[data['MRI Subchondral cyst - Femur central compartment'] == 'No', 'MRI Subchondral cyst - Femur central compartment'] = False
+data.loc[data['MRI Subchondral cyst - Femur central compartment'] == 'Yes', 'MRI Subchondral cyst - Femur central compartment'] = True
 
-data.loc[data['MRI Subchondral cyst - Femur peripheral compartment'] == 'Male', 'Sex'] = 0
-data.loc[data['MRI Subchondral cyst - Femur peripheral compartment'] == 'Female', 'Sex'] = 1
+data.loc[data['MRI Subchondral cyst - Femur peripheral compartment'] == 'No', 'MRI Subchondral cyst - Femur peripheral compartment'] = False
+data.loc[data['MRI Subchondral cyst - Femur peripheral compartment'] == 'Yes', 'MRI Subchondral cyst - Femur peripheral compartment'] = True
 
-data.loc[data['MRI Subchondral cyst - Acetabulum central compartment'] == 'Male', 'Sex'] = 0
-data.loc[data['MRI Subchondral cyst - Acetabulum central compartment'] == 'Female', 'Sex'] = 1
+data.loc[data['MRI Subchondral cyst - Acetabulum central compartment'] == 'No', 'MRI Subchondral cyst - Acetabulum central compartment'] = False
+data.loc[data['MRI Subchondral cyst - Acetabulum central compartment'] == 'Yes', 'MRI Subchondral cyst - Acetabulum central compartment'] = True
 
-data.loc[data['MRI Perilabral cyst'] == 'Male', 'Sex'] = 0
-data.loc[data['MRI Perilabral cyst'] == 'Female', 'Sex'] = 1
+data.loc[data['MRI Perilabral cyst'] == 'No', 'MRI Perilabral cyst'] = False
+data.loc[data['MRI Perilabral cyst'] == 'Yes', 'MRI Perilabral cyst'] = True
 
-data.loc[data['MRI Capsular Laxity'] == 'Male', 'Sex'] = 0
-data.loc[data['MRI Capsular Laxity'] == 'Female', 'Sex'] = 1
+data.loc[data['MRI Capsular Laxity'] == 'No', 'MRI Capsular Laxity'] = False
+data.loc[data['MRI Capsular Laxity'] == 'Yes', 'MRI Capsular Laxity'] = True
 
-data.loc[data['Coxa Profunda (Pre-op)'] == 'Male', 'Sex'] = 0
-data.loc[data['Coxa Profunda (Pre-op)'] == 'Female', 'Sex'] = 1
+data.loc[data['Coxa Profunda (Pre-op)'] == 'No', 'Coxa Profunda (Pre-op)'] = False
+data.loc[data['Coxa Profunda (Pre-op)'] == 'Yes', 'Coxa Profunda (Pre-op)'] = True
+"""
+data.loc[data['Gait'] == 'No', 'Gait'] = False
+data.loc[data['Gait'] == 'Yes', 'Gait'] = True
+"""
 
-data.loc[data['Gait'] == 'Male', 'Sex'] = 0
-data.loc[data['Gait'] == 'Female', 'Sex'] = 1
+data.loc[data['Anterior Impinge'] == 'Negative', 'Anterior Impinge'] = 0
+data.loc[data['Anterior Impinge'] == 'Positive', 'Anterior Impinge'] = 1
+data.loc[data['Anterior Impinge'] == 'Mildly Positive', 'Anterior Impinge'] = 2
 
-data.loc[data['Anterior Impinge'] == 'Male', 'Sex'] = 0
-data.loc[data['Anterior Impinge'] == 'Female', 'Sex'] = 1
+data.loc[data['Lateral Imping'] == ' Negative', 'Lateral Imping'] = False
+data.loc[data['Lateral Imping'] == 'Positive', 'Lateral Imping'] = True
 
-data.loc[data['Lateral Imping'] == 'Male', 'Sex'] = 0
-data.loc[data['Lateral Imping'] == 'Female', 'Sex'] = 1
+data.loc[data['Internal Snapping'] == 'Negative', 'Internal Snapping'] = False
+data.loc[data['Internal Snapping'] == 'Positive', 'Internal Snapping'] = True
 
-data.loc[data['Internal Snapping'] == 'Male', 'Sex'] = 0
-data.loc[data['Internal Snapping'] == 'Female', 'Sex'] = 1
-
-data.loc[data['External Snapp'] == 'Male', 'Sex'] = 0
-data.loc[data['External Snapp'] == 'Female', 'Sex'] = 1
+data.loc[data['External Snapp'] == 'Negative', 'External Snapp'] = False
+data.loc[data['External Snapp'] == 'Positive', 'External Snapp'] = True
 #d_sex = {'Female': True, 'Male': False}
 #d_cond = {'Yes': True, 'No':False}
 print(data.head())
+data.to_csv('dataformat.csv')
